6e903a1683b4568e344427c961c391a817deb291
P1xel10 <154343913+P1xel10@users.noreply.github.com>
2024-08-16 15:59:11 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const nggUrl = 'https://mathsspot.com';
+
+const proxy = createProxyMiddleware({
+  target: https://mathsspot.com,
+  changeOrigin: true,
+  secure: true,
+  logLevel: 'info', // Adjust the log level as needed
+});
+
+// Remove unnecessary headers
+proxy.on('proxyReq', (proxyReq, req) => {
+  if (req.headers.host === 'mathsspot.com') {
+    proxyReq.removeHeader('X-Forwarded-For');
+    proxyReq.removeHeader('X-Real-IP');
+    proxyReq.removeHeader('Via');
+  }
+});
+
+app.use('/', proxy);
+
+const port = process.env.PORT || 443;
+app.listen(port, () => {
+  console.log(`CybriaGG is running on port ${port}`);
+});
