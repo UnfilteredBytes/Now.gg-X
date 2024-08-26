3a1c005683ba6481a08a869c9a93b0c57cf3d240
Pxl12 <x0p.dosv7r@gmail.com>
2024-08-26 11:13:10 -0400

    Create clerk.js

---
+++ .env.local/clerk.js
@@ -0,0 +1,46 @@
+npm install @clerk/nextjs
+
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_anVzdC1tYXN0b2Rvbi01LmNsZXJrLmFjY291bnRzLmRldiQ
+CLERK_SECRET_KEY=pk_test_anVzdC1tYXN0b2Rvbi01LmNsZXJrLmFjY291bnRzLmRldiQ
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
